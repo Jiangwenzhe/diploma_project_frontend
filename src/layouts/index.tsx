9e0ef2e1350fdea60270363d6175e00e812c61f6
Erzhuyijian <1821344102@qq.com>
2020-03-25 16:40:07 +0800

    index layout 更改导出方式

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PageHeader from '@/components/header/index';
+import { Button } from 'antd';
+import { history } from 'umi';
 
-export default (props: any) => {
-  // console.log('layout props', props);
+const BasicLayout = (props: any) => {
+  console.log('layout props', props);
   const {
     location: { pathname },
     children,
@@ -10,7 +12,12 @@ export default (props: any) => {
   return (
     <div>
       <PageHeader pathname={pathname} />
+      <Button onClick={() => {
+        history.goBack()
+      }}>go back</Button>
         <div style={{ padding: 20 }}>{ children }</div>
     </div>
   );
 }
+
+export default BasicLayout;
