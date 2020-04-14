a46edd9eae2209d1a665f978eab749b2e5bf3ab9
Erzhuyijian <1821344102@qq.com>
2020-04-14 22:24:16 +0800

    layouts -> index.jsx 中包含 useEffect ，其中 dispatch 获取 user/fetchCurrent  获取用户信息

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,23 +2,33 @@ import React, {useEffect} from 'react';
 import PageHeader from '../components/header/index.jsx';
 import { useRequest } from '@umijs/hooks';
 import { Button } from 'antd';
-// import { history } from 'umi';
+import { connect } from 'umi';
 const BasicLayout = (props) => {
-  console.log('layout props', props);
-  const { data, error, loading } = useRequest({
-    url: '/api/user/5e705038256ed078616221c1',
-    method: 'get',
-  });
   const {
     location: { pathname },
     children,
+    dispatch,
+    user,
+    login
   } = props;
+
+  useEffect(() => {
+    if (dispatch && localStorage.getItem('node-oj-token')) {
+      dispatch({
+        type: 'user/fetchCurrent',
+      });
+    }
+  }, [login]);
+
   return (
     <div>
-      <PageHeader pathname={pathname} />
+      <PageHeader pathname={pathname} user={user}/>
       <div style={{ padding: 20 }}>{children}</div>
     </div>
   );
 };
 
-export default BasicLayout;
+export default connect(({ user, login }) => ({
+  user,
+  login
+}))(BasicLayout);
