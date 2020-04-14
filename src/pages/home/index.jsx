89f4d3e2ab416dadbe6beabf2d2a65a7b5f0a80f
Erzhuyijian <1821344102@qq.com>
2020-04-14 22:21:39 +0800

    page -> home 测试 useAccess() 函数

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,8 +3,8 @@ import styles from './index.less';
 import { useAccess } from 'umi';
 
 export default () => {
-  const access = useAccess();
-  console.log(access);
+  // const access = useAccess();
+  // console.log(access);
   return (
     <div>
       <h1>Home</h1>
