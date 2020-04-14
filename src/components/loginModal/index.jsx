8796f3050f094dc756a283d8a85846b8533a10eb
Erzhuyijian <1821344102@qq.com>
2020-04-14 22:27:15 +0800

    添加 loginModal 登录组件

---
+++ src/components/loginModal/index.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { connect } from 'umi';
+import {
+  Menu,
+  Row,
+  Col,
+  Avatar,
+  Typography,
+  Dropdown,
+  Button,
+  Modal,
+  Form,
+  Input,
+  Checkbox,
+  message,
+} from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { span: 19 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 2, span: 22 },
+};
+
+const LoginModal = props => {
+  const {
+    visible,
+    hideVisible,
+    dispatch,
+    submitting,
+    login: { status },
+  } = props;
+
+  const onFinish = values => {
+    dispatch({ type: 'login/login', payload: { ...values } }).then(status => {
+      if (status === 'ok') {
+        hideVisible();
+        message.success(`👏 ${values.name}, 欢迎回来 👏`);
+      }
+    });
+  };
+
+  // const onFinishFailed = errorInfo => {
+  //   console.log('Failed:', errorInfo);
+  // };
+
+  return (
+    <Modal
+      title="🤩用户登录"
+      visible={visible}
+      // onOk={handleOk}
+      onCancel={hideVisible}
+      footer={null}
+    >
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="用户名"
+          name="name"
+          rules={[{ required: true, message: '请输入用户名!' }]}
+        >
+          <Input prefix={<UserOutlined />} />
+        </Form.Item>
+
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: '请输入用密码!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit" loading={submitting} block>
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default connect(({ login, loading }) => ({
+  login,
+  submitting: loading.effects['login/login'],
+}))(LoginModal);
