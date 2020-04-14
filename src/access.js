628df9e51e320aedc88962bd63d3e6ab1e673e14
Erzhuyijian <1821344102@qq.com>
2020-04-14 22:26:23 +0800

    添加 useAccess 配合 src/app.js 使用

--- src/access.js
+++ src/access.js
@@ -2,18 +2,11 @@
  * @Author: Wenzhe
  * @Date: 2020-04-14 12:35:19
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-14 12:55:20
+ * @LastEditTime: 2020-04-14 19:00:33
  */
 export default function(initialState) {
-  const { userId, role } = { userId: 1, role: 'admin'};
-
   return {
-    userId,
-    role,
-    // canReadFoo: true,
-    // canUpdateFoo: role === 'admin',
-    // canDeleteFoo: foo => {
-    //   return foo.ownerId === userId;
-    // },
+    userInfo: initialState,
+    canUpdate: initialState ? initialState.privilege === 3 : false,
   };
 }
--- src/app.js
+++ src/app.js
@@ -2,5 +2,17 @@
  * @Author: Wenzhe
  * @Date: 2020-03-20 14:03:23
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-20 14:03:24
+ * @LastEditTime: 2020-04-14 18:59:44
  */
+
+import { queryCurrent } from './service/user';
+
+export async function getInitialState() {
+  if (!localStorage.getItem('node-oj-token')) {
+    return {
+      msg: '用户未登录',
+    };
+  }
+  const response = await queryCurrent();
+  return response.data;
+}
