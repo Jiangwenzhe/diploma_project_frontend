cf0029032763f8426312fc9caa9ba1880add2e4d
Erzhuyijian <1821344102@qq.com>
2020-04-15 18:26:05 +0800

    add problemList.js 作为题目列表展示页面

--- src/pages/problem/problemList/index.jsx
+++ src/pages/problem/problemList/index.jsx
@@ -1,10 +1,225 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Card,
+  Typography,
+  Input,
+  Table,
+  Button,
+  Popconfirm,
+  Statistic,
+  Row,
+  Col,
+  Tag,
+  Divider,
+  Menu,
+  Dropdown,
+} from 'antd';
 import styles from './index.less';
+import request from '@/utils/request';
+import { connect, Link } from 'umi';
+import {
+  TagOutlined,
+  createFromIconfontCN,
+  DownOutlined,
+} from '@ant-design/icons';
+import moment from 'moment';
 
-export default () => {
+const { Text, Title } = Typography;
+const { CheckableTag } = Tag;
+const { Search } = Input;
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1756231_o0ugrzj2f9f.js',
+});
+
+const difficultyToTag = difficulty => {
+  if (!difficulty) return <Tag color="success">简单</Tag>;
+  switch (difficulty) {
+    case 'Low':
+      return <Tag color="success">简单</Tag>;
+    case 'Med':
+      return <Tag color="success">中等</Tag>;
+    case 'High':
+      return <Tag color="success">困难</Tag>;
+    default:
+      return <Tag color="success">简单</Tag>;
+  }
+};
+
+const ProblemList = props => {
+  const {
+    problem: { problemList, total },
+    dispatch,
+    fetching,
+  } = props;
+
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+  });
+  const [query, setQuery] = useState('');
+  const [tags, setTags] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  useEffect(() => {
+    request('/api/problemtag').then(response => {
+      const tagValue = response.data.map(item => item.name);
+      setTags(tagValue);
+    });
+  }, []);
+
+  useEffect(() => {
+    dispatch({
+      type: 'problem/fetchProblemList',
+      payload: { pagination, query },
+    });
+  }, [dispatch, pagination, query]);
+
+  // params: (current_pagination, filters, sorter)
+  const tableChangeHandler = current_pagination => {
+    setPagination(current_pagination);
+  };
+
+  const handleTitleQuery = value => {
+    setQuery({ ...query, title: value });
+    setPagination({ current: 1, pageSize: 10 });
+  };
+
+  const handleProblemTagSelected = (tag, checked) => {
+    console.log(tag, checked);
+    // const
+    const nextSelectedTags = checked
+      ? [...selectedTags, tag]
+      : selectedTags.filter(t => t !== tag);
+    setSelectedTags(nextSelectedTags);
+  };
+
+  const columns = [
+    {
+      title: 'isFinished',
+      dataIndex: 'pid',
+      key: 'pid',
+      width: '10%',
+    },
+    {
+      title: '#',
+      dataIndex: 'pid',
+      key: 'pid',
+      width: '10%',
+    },
+    {
+      title: '题名',
+      dataIndex: 'title',
+      render: value => <Text strong>{value}</Text>,
+      width: '50%',
+    },
+    {
+      title: '题解',
+      render: value => {
+        return value.submit;
+      },
+      width: '10%',
+    },
+    {
+      title: '通过率',
+      render: value => {
+        const rate = (value.solve / value.submit) * 100;
+        // return <Statistic value={isNaN(rate) ? 0 : rate} suffix="%"/>
+        return <>{`${isNaN(rate) ? 0 : rate}%`}</>;
+      },
+      width: '10%',
+    },
+    {
+      title: '难度',
+      render: value => {
+        return difficultyToTag(value.difficulty);
+      },
+      width: '10%',
+    },
+  ];
+
+  const select_difficulty_menu = (
+    <Menu>
+      <Menu.Item>简单</Menu.Item>
+      <Menu.Item>中等</Menu.Item>
+      <Menu.Item>困难</Menu.Item>
+    </Menu>
+  );
+
   return (
     <div>
-      <h1>Problems</h1>
+      <Row>
+        <Col span={15} offset={2}>
+          <Table
+            columns={columns}
+            rowKey="_id"
+            dataSource={problemList}
+            pagination={{ ...pagination, total, showSizeChanger: true }}
+            onChange={tableChangeHandler}
+            loading={fetching}
+            title={() => (
+              <Row align="middle">
+                <Col span={4}>
+                  <Title level={4}>题目列表</Title>
+                </Col>
+                <Col offset={12} span={4}>
+                  <Search />
+                </Col>
+                <Col span={1} offset={1}>
+                  <Dropdown overlay={select_difficulty_menu}>
+                    <a
+                      className="ant-dropdown-link"
+                      onClick={e => e.preventDefault()}
+                    >
+                      难度 <DownOutlined />
+                    </a>
+                  </Dropdown>
+                </Col>
+              </Row>
+            )}
+          />
+        </Col>
+        <Col span={4} offset={1}>
+          <Row>
+            <div className={styles.section_title}>
+              <IconFont type="icon-tag-o" />
+              <span style={{ marginLeft: '10px' }}>标签分类</span>
+              <br />
+              <div className={styles.tag_group}>
+                {tags.map(tag => {
+                  return (
+                    <CheckableTag
+                      checked={selectedTags.indexOf(tag) > -1}
+                      onChange={checked =>
+                        handleProblemTagSelected(tag, checked)
+                      }
+                      key={tag}
+                    >
+                      {tag}
+                    </CheckableTag>
+                  );
+                })}
+              </div>
+            </div>
+          </Row>
+          <Divider />
+          <Row>
+            <div>
+              <div className={styles.section_header}>
+                <IconFont type="icon-schedule" />
+                <span style={{ marginLeft: '10px' }}>我的进度</span>
+              </div>
+              会有一个饼图放在这里
+            </div>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
-}
+};
+
+export default connect(({ problem, loading }) => ({
+  problem,
+  fetching: loading.effects['problem/fetchProblemList'],
+}))(ProblemList);
