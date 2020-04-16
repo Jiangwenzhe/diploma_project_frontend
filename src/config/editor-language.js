4fc2efaa7c65695ecdecd2284ff46ce5d60cd9fd
Erzhuyijian <1821344102@qq.com>
2020-04-16 16:20:20 +0800

    添加 编辑器 语言与 theme 的设置

---
+++ src/config/editor-language.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-04-16 15:55:14
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-04-16 16:02:36
+ */
+const editor_language = [
+  {
+    language: 'C++',
+    name: 'C++',
+  },
+  {
+    language: 'C',
+    name: 'C',
+  },
+  {
+    language: 'Java',
+    name: 'Java',
+  },
+  {
+    language: 'Python2',
+    name: 'Python2',
+  },
+  {
+    language: 'Python3',
+    name: 'Python3',
+  },
+];
+
+export default editor_language;
---
+++ src/config/editor-theme.js
@@ -0,0 +1,58 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-04-16 14:37:38
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-04-16 16:00:07
+ */
+const custom_editor_theme = [
+  'Active4D',
+  'All Hallows Eve',
+  'Amy',
+  'Birds of Paradise',
+  'Blackboard',
+  'Brilliance Black',
+  'Brilliance Dull',
+  'Chrome DevTools',
+  'Clouds Midnight',
+  'Clouds',
+  'Cobalt',
+  'Dawn',
+  'Dominion Day',
+  'Dreamweaver',
+  'Eiffel',
+  'Espresso Libre',
+  'GitHub',
+  'IDLE',
+  'Katzenmilch',
+  'Kuroir Theme',
+  'LAZY',
+  'Merbivore Soft',
+  'Merbivore',
+  'Monokai Bright',
+  'Monokai',
+  'Night Owl',
+  'Oceanic Next',
+  'Pastels on Dark',
+  'Slush and Poppies',
+  'Solarized-dark',
+  'Solarized-light',
+  'SpaceCadet',
+  'Sunburst',
+  'Tomorrow-Night-Blue',
+  'Tomorrow-Night-Bright',
+  'Tomorrow-Night-Eighties',
+  'Tomorrow-Night',
+  'Tomorrow',
+  'Twilight',
+  'Upstream Sunburst',
+  'Vibrant Ink',
+  'Zenburnesque',
+  'iPlastic',
+  'idleFingers',
+  'krTheme',
+  'monoindustrial',
+];
+
+const default_editor_theme = ['light', 'dark'];
+
+export { custom_editor_theme, default_editor_theme };
