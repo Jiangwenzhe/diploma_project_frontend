a407e0c7bcde16cddd056129940c0f6fc96ab648
Erzhuyijian <1821344102@qq.com>
2020-04-17 21:52:55 +0800

    add config/judge_result.js 添加 judge_result 的配置文件

---
+++ src/config/judge_result.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-04-17 12:21:07
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-04-17 12:24:50
+ */
+const judge_result = {
+  '-2': 'Complie Error',
+  '-1': 'Wrong Answer',
+  '0': 'Success',
+  '1': 'CPU TIME LIMIT EXCEEDED',
+  '2': 'REAL TIME LIMIT EXCEEDED',
+  '3': 'MEMORY LIMIT EXCEEDED',
+  '4': 'RUNTIME ERROR',
+  '5': 'SYSTEM ERROR',
+};
+
+export { judge_result };
