18733cac86a756701f04b9f7ecdbd916badbeede
Erzhuyijian <1821344102@qq.com>
2020-04-21 20:59:46 +0800

    添加 iconfont url config

---
+++ src/config/iconfont.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-04-21 20:54:50
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-04-21 20:55:04
+ */
+
+export default '//at.alicdn.com/t/font_1756231_75knjkwwy3e.js';
