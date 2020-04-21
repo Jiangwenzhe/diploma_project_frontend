02ec538c48dd474e97db39b56d91b1d7415f6a2e
Erzhuyijian <1821344102@qq.com>
2020-04-21 16:52:02 +0800

    config / judge_result 更改 0 的 value 从 Success => Accept

--- src/config/judge_result.js
+++ src/config/judge_result.js
@@ -2,12 +2,12 @@
  * @Author: Wenzhe
  * @Date: 2020-04-17 12:21:07
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-17 12:24:50
+ * @LastEditTime: 2020-04-21 10:35:39
  */
 const judge_result = {
   '-2': 'Complie Error',
   '-1': 'Wrong Answer',
-  '0': 'Success',
+  '0': 'Accepted',
   '1': 'CPU TIME LIMIT EXCEEDED',
   '2': 'REAL TIME LIMIT EXCEEDED',
   '3': 'MEMORY LIMIT EXCEEDED',
