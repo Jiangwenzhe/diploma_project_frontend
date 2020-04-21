5807445e6fbd306ff91b0b528798e7ed2a41d04f
Erzhuyijian <1821344102@qq.com>
2020-04-21 16:48:30 +0800

    add config / language_template.js 添加各个语言的模版

---
+++ src/config/language_template.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-04-20 10:55:44
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-04-20 15:54:53
+ */
+const cpp_template = `// this is a C++ template
+
+`;
+
+const c_template = `// this is a C template
+
+`;
+
+const java_template = `// this is a Java template
+
+`;
+
+const py_2_template = `# this is a Python2 template
+
+`;
+
+const py_3_template = `# this is a Python3 template
+
+`;
+
+export {
+  cpp_template,
+  c_template,
+  java_template,
+  py_2_template,
+  py_3_template,
+};
