5fc3ef1f584231fe3db63938599661f0220a6fea
Erzhuyijian <1821344102@qq.com>
2020-04-22 19:30:21 +0800

    修改 header 的样式

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Menu, Row, Col, Avatar, Typography, Button } from 'antd';
+import { Menu, Row, Col, Avatar, Typography, Button, Layout } from 'antd';
 import styles from './index.less';
 import { Link, connect } from 'umi';
 import LoginModal from '../loginModal';
 
 const { Item, SubMenu } = Menu;
 const { Text } = Typography;
+const { Header } = Layout;
 
 const menuData = [
   { route: '/', name: 'Home' },
@@ -32,64 +33,70 @@ const PageHeader = (props) => {
 
   return (
     <>
-      <Row className={styles.header} align="middle">
-        <Col span={3}>{/* logo */}</Col>
-        <Col span={11}>
-          <Menu
-            mode="horizontal"
-            style={{ lineHeight: '60px' }}
-            selectedKeys={[`/${pathname.split('/')[1]}`]}
-          >
-            {menuData.map((menu) => {
-              return (
-                <Item key={menu.route}>
-                  <Link to={menu.route}>{menu.name}</Link>
-                </Item>
-              );
-            })}
-          </Menu>
-        </Col>
-        <Col offset={7}>
-          {!currentUser.uid ? (
-            <>
-              <Button
-                type="dashed"
-                size="small"
-                onClick={() => setLoginModelVisible(true)}
-              >
-                登录
-              </Button>
-              <Button style={{ marginLeft: '10px' }} type="dashed" size="small">
-                注册
-              </Button>
-            </>
-          ) : (
+      <div className={styles.header}>
+        <Row align="middle">
+          <Col span={3}>{/* logo */}</Col>
+          <Col span={11}>
             <Menu
-              style={{ lineHeight: '60px' }}
+              className={styles.header_menu}
               mode="horizontal"
-              selectable={false}
+              selectedKeys={[`/${pathname.split('/')[1]}`]}
             >
-              <SubMenu
-                title={
-                  <>
-                    <Avatar size="small" src={currentUser.avatar} />
-                    <Text style={{ marginLeft: '10px' }}>
-                      {currentUser.name}
-                    </Text>
-                  </>
-                }
-              >
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
-                <Menu.Item key="setting:3">Option 3</Menu.Item>
-                <Menu.Item key="logout" onClick={() => logout()}>
-                  退出登录
-                </Menu.Item>
-              </SubMenu>
+              {menuData.map((menu) => {
+                return (
+                  <Item key={menu.route}>
+                    <Link to={menu.route}>{menu.name}</Link>
+                  </Item>
+                );
+              })}
             </Menu>
-          )}
-        </Col>
-      </Row>
+          </Col>
+          <Col offset={7}>
+            {!currentUser.uid ? (
+              <>
+                <Button
+                  type="dashed"
+                  size="small"
+                  onClick={() => setLoginModelVisible(true)}
+                >
+                  登录
+                </Button>
+                <Button
+                  style={{ marginLeft: '10px' }}
+                  type="dashed"
+                  size="small"
+                >
+                  注册
+                </Button>
+              </>
+            ) : (
+              <Menu
+                mode="horizontal"
+                selectable={false}
+                className={styles.user_menu}
+              >
+                <SubMenu
+                  title={
+                    <>
+                      <Avatar size="small" src={currentUser.avatar} />
+                      <Text style={{ marginLeft: '10px' }}>
+                        {currentUser.name}
+                      </Text>
+                    </>
+                  }
+                >
+                  <Menu.Item key="setting:1">Option 1</Menu.Item>
+                  <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  <Menu.Item key="setting:3">Option 3</Menu.Item>
+                  <Menu.Item key="logout" onClick={() => logout()}>
+                    退出登录
+                  </Menu.Item>
+                </SubMenu>
+              </Menu>
+            )}
+          </Col>
+        </Row>
+      </div>
       <LoginModal
         visible={loginModelVisible}
         hideVisible={() => setLoginModelVisible(false)}
