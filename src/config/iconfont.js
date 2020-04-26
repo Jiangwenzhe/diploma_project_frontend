559d7a1e619ec0e8821ef35bec8c6402b88f66e7
Erzhuyijian <1821344102@qq.com>
2020-04-26 14:19:01 +0800

    更新 config / iconfont.js 中的 icon url

--- src/config/iconfont.js
+++ src/config/iconfont.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-04-21 20:54:50
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-21 20:55:04
+ * @LastEditTime: 2020-04-26 11:54:06
  */
 
-export default '//at.alicdn.com/t/font_1756231_75knjkwwy3e.js';
+export default '//at.alicdn.com/t/font_1756231_zenytroiewk.js';
