044c3da3070e80933b98ab07bd11c08e7658f6e2
Erzhuyijian <1821344102@qq.com>
2020-04-27 10:07:36 +0800

    service / userInfo 添加 updateUserInfo 方法

--- src/service/userInfo.js
+++ src/service/userInfo.js
@@ -2,10 +2,17 @@
  * @Author: Wenzhe
  * @Date: 2020-04-25 16:32:03
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-25 16:44:16
+ * @LastEditTime: 2020-04-27 09:37:52
  */
 import request from '../utils/request';
 
 export async function fetchUserInfo(params) {
   return request(`/api/userinfo/${params.uid}`);
+}
+
+export async function updateUserInfo(params) {
+  return request(`/api/user/${params.id}`, {
+    method: 'PUT',
+    data: params.data,
+  });
 }
