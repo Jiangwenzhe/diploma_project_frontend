ff8cf717dbc5678064bb458242fedba1fcd84aaa
Erzhuyijian <1821344102@qq.com>
2020-04-30 22:14:24 +0800

    在 tool_fuc.js 中添加数组 filter 与 inverseFilter 函数

--- src/utils/tool_fuc.js
+++ src/utils/tool_fuc.js
@@ -2,10 +2,21 @@
  * @Author: Wenzhe
  * @Date: 2020-04-21 10:48:38
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-21 16:47:32
+ * @LastEditTime: 2020-04-30 12:49:13
  */
 
 //  bit 转 mb 的函数
-const BtoMB = param => Math.round(param / (1024 * 1024));
+const BtoMB = (param) => Math.round(param / (1024 * 1024));
 
-export { BtoMB };
+// 数组 filter 函数
+const filterItem = (arr, query) => {
+  return arr.filter((el) => el.toLowerCase().indexOf(query.toLowerCase()) > -1);
+};
+
+const inverseFilter = (arr, query) => {
+  return arr.filter(
+    (el) => el.toLowerCase().indexOf(query.toLowerCase()) === -1,
+  );
+};
+
+export { BtoMB, filterItem, inverseFilter };
