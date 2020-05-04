1d70648022de1cdf5ef615bd8a68d60eb65a70b8
Erzhuyijian <1821344102@qq.com>
2020-05-04 09:32:15 +0800

    添加 config / contest_config 的配置文件

---
+++ src/config/contest_config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-05-03 19:11:02
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-05-04 09:31:48
+ */
+import { tag } from 'antd';
+
+const makeContestTypeTag = (has_pass) => {
+  return null;
+};
