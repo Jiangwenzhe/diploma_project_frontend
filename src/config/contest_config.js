ec8defd6cde2785d085e027a8aa585a8744320de
Erzhuyijian <1821344102@qq.com>
2020-05-06 18:02:48 +0800

    contest_config 添加 makeStrokeColor 方法

--- src/config/contest_config.js
+++ src/config/contest_config.js
@@ -2,10 +2,37 @@
  * @Author: Wenzhe
  * @Date: 2020-05-03 19:11:02
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-05-04 09:31:48
+ * @LastEditTime: 2020-05-06 08:39:05
  */
-import { tag } from 'antd';
 
-const makeContestTypeTag = (has_pass) => {
-  return null;
+const makeStrokeColor = (rate) => {
+  if (rate <= 25) {
+    return '#f5222d';
+  }
+  if (rate > 25 && rate <= 50) {
+    return '#fa8c16';
+  }
+  if (rate > 50 && rate <= 75) {
+    return '#1890ff';
+  }
+  if (rate > 75) {
+    return '#52c41a';
+  }
 };
+
+const makeReverseStrokeColor = (rate) => {
+  if (rate <= 25) {
+    return '#52c41a';
+  }
+  if (rate > 25 && rate <= 50) {
+    return '#1890ff';
+  }
+  if (rate > 50 && rate <= 75) {
+    return '#fa8c16';
+  }
+  if (rate > 75) {
+    return '#f5222d';
+  }
+};
+
+export { makeStrokeColor, makeReverseStrokeColor };
