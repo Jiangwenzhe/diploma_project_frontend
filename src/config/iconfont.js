9673dfd319fe9a10cf39c5ba7294c96a6870a7a9
Erzhuyijian <1821344102@qq.com>
2020-05-06 18:03:42 +0800

    更新 iconfont.js

--- src/config/iconfont.js
+++ src/config/iconfont.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-04-21 20:54:50
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-05-01 16:52:38
+ * @LastEditTime: 2020-05-06 16:12:02
  */
 
-export default '//at.alicdn.com/t/font_1756231_fy41a8inikn.js';
+export default '//at.alicdn.com/t/font_1756231_xa7zmxq3uil.js';
