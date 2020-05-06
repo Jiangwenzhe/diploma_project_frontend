300ef3e6ba8c455862fdc7c08d7d00ba1e7e99da
Erzhuyijian <1821344102@qq.com>
2020-05-06 18:04:15 +0800

    service 添加 contest 的方法

--- src/service/contest.js
+++ src/service/contest.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-05-03 14:07:32
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-05-03 23:47:53
+ * @LastEditTime: 2020-05-06 10:07:07
  */
 import request from '@/utils/request';
 
@@ -22,9 +22,13 @@ export async function getContentDetail(params) {
 }
 
 export async function verifyUserPersmission(params) {
-  console.log('params', params);
   return request('/api/contest/verify', {
     method: 'POST',
     data: params,
   });
+}
+
+export async function getProblemInfoByCidAndPid(params) {
+  const { cid, pid } = params;
+  return request(`/api/contest_problemDetail?cid=${cid}&pid=${pid}`);
 }
