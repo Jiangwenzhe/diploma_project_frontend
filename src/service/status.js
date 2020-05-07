1fefbf8798c85afe841ccd44d7a53f0de87a0110
Erzhuyijian <1821344102@qq.com>
2020-05-07 11:59:12 +0800

    status 中的 service 请求的 query 修改

--- src/service/status.js
+++ src/service/status.js
@@ -2,17 +2,17 @@
  * @Author: Wenzhe
  * @Date: 2020-04-22 09:34:08
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-24 09:56:45
+ * @LastEditTime: 2020-05-07 11:28:49
  */
 import request from '@/utils/request';
 
 export async function getStatusList(params) {
   const {
     pagination: { current, pageSize },
-    query: { uid, pid },
+    query: { uid, pid, username, result, language },
   } = params;
   return request(
-    `/api/submission?current=${current}&pageSize=${pageSize}&uid=${uid}&pid=${pid}`,
+    `/api/submission?current=${current}&pageSize=${pageSize}&uid=${uid}&pid=${pid}&username=${username}&result=${result}&language=${language}`,
   );
 }
 
