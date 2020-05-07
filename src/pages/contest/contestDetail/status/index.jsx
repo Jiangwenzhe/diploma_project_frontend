bf5d113fe0a7524b3baa137bdccff1d2cf02c9f7
Erzhuyijian <1821344102@qq.com>
2020-05-07 11:09:06 +0800

    完成 contest 中的 contestDetail 界面

--- src/pages/contest/contestDetail/status/index.jsx
+++ src/pages/contest/contestDetail/status/index.jsx
@@ -1,3 +1,205 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Row, Col, Input, Form, Button, Select } from 'antd';
+import { connect, Link } from 'umi';
+import styles from './index.less';
+import StatusTag from '../../../../components/StatusTag';
+import moment from 'moment';
+import { BtoMB } from '../../../../utils/tool_fuc';
 
-export default () => <h1>Status</h1>;
+const { Item } = Form;
+const { Option } = Select;
+
+const StatusList = (props) => {
+  const {
+    match,
+    dispatch,
+    fetchStatusListLoading,
+    contestDetail: { statusList, statusTotal },
+  } = props;
+
+  const [form] = Form.useForm();
+
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+  });
+
+  const [query, setQuery] = useState({
+    uid: '',
+    pid: '',
+    status: 2,
+    cid: match.params.cid,
+    username: '',
+    result: '',
+    language: '',
+  });
+
+  const [pid, setPid] = useState('');
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    dispatch({
+      type: 'contestDetail/fetchStatusList',
+      payload: { pagination, query },
+    });
+  }, [dispatch, pagination, query]);
+
+  const tableChangeHandler = (current_pagination) => {
+    setPagination(current_pagination);
+  };
+
+  const handleFormFinish = (values) => {
+    console.log('Finish:', values);
+    const { pid, name, result, language } = values;
+    setQuery({
+      ...query,
+      pid,
+      name,
+      result: result ? result : '',
+      language: language ? language : '',
+    });
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: '_id',
+      render: (id) => {
+        return <Link to={`/status/${id}`}>{id}</Link>;
+      },
+    },
+    {
+      title: 'PID',
+      dataIndex: 'pid',
+    },
+    {
+      title: '提交用户',
+      render: (_, record) => {
+        return record.userInfo.name;
+      },
+    },
+    {
+      title: '判题结果',
+      dataIndex: 'result',
+      render: (result) => <StatusTag status={result} />,
+    },
+    {
+      title: 'CPU 用时',
+      // dataIndex: 'cpu_time_cost',
+      render: (record) => {
+        if (record.result === -2) return '/';
+        return record.status_info.cpu_time_cost;
+      },
+    },
+    {
+      title: '执行用时',
+      // dataIndex: 'real_time__cost',
+      render: (record) => {
+        if (record.result === -2) return '/';
+        return record.status_info.real_time__cost;
+      },
+    },
+    {
+      title: '内存消耗',
+      // dataIndex: 'memory_cost',
+      render: (record) => {
+        if (record.result === -2) return '/';
+        return `${BtoMB(record.status_info.memory_cost)} MB`;
+      },
+    },
+    {
+      title: '执行语言',
+      dataIndex: 'language',
+    },
+    {
+      title: '提交时间',
+      dataIndex: 'create_at',
+      render: (time_stamp) => moment(time_stamp).format('YYYY-MM-DD HH:mm:ss'),
+    },
+  ];
+
+  const formItemLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 17 },
+  };
+
+  const buttonItemLayout = {
+    wrapperCol: { span: 14, offset: 4 },
+  };
+
+  return (
+    <div>
+      <div>
+        <Form
+          layout="inline"
+          form={form}
+          initialValues={{
+            pid: '',
+            name: '',
+          }}
+          onFinish={handleFormFinish}
+        >
+          <Item label="Pid" name="pid">
+            <Input placeholder="请输入 pid" allowClear />
+          </Item>
+          <Item label="用户名" name="name">
+            <Input placeholder="请输入用户名" allowClear />
+          </Item>
+          <Item name="result" label="判题结果">
+            <Select
+              style={{ width: 200 }}
+              placeholder="请选择判题结果"
+              allowClear={true}
+            >
+              <Option value="0">Accepted</Option>
+              <Option value="-1">Wrong Answer</Option>
+              <Option value="-2">Complie Error</Option>
+              <Option value="4">RUNTIME ERROR</Option>
+              <Option value="5">SYSTEM ERROR</Option>
+              <Option value="1">CPU TIME LIMIT EXCEEDED</Option>
+              <Option value="2">REAL TIME LIMIT EXCEEDED</Option>
+            </Select>
+          </Item>
+          <Item name="language" label="执行语言">
+            <Select
+              style={{ width: 200 }}
+              placeholder="请选择执行语言"
+              allowClear={true}
+            >
+              <Option value="Cpp">C++</Option>
+              <Option value="C">C</Option>
+              <Option value="Java">Java</Option>
+              <Option value="Python2">Python2</Option>
+              <Option value="Python3">Python3</Option>
+            </Select>
+          </Item>
+          <Item {...buttonItemLayout}>
+            <Button type="dashed" htmlType="submit">
+              搜索
+            </Button>
+          </Item>
+        </Form>
+      </div>
+      <div style={{ marginTop: '20px' }}>
+        <Table
+          columns={columns}
+          rowKey="_id"
+          onChange={tableChangeHandler}
+          dataSource={statusList}
+          pagination={{
+            ...pagination,
+            total: statusTotal,
+            showSizeChanger: true,
+          }}
+          loading={fetchStatusListLoading}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default connect(({ contestDetail, loading }) => ({
+  contestDetail,
+  fetchStatusListLoading: loading.effects['contestDetail/fetchStatusList'],
+}))(StatusList);
