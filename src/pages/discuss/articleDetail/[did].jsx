8451e3927385b865ddfad4de8fdf4e1b4f9a111c
Erzhuyijian <1821344102@qq.com>
2020-06-07 18:17:25 +0800

    优化 discussDetail 页面

--- src/pages/discuss/articleDetail/[did].jsx
+++ src/pages/discuss/articleDetail/[did].jsx
@@ -2,14 +2,22 @@
  * @Author: Wenzhe
  * @Date: 2020-04-30 16:25:16
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-05-12 08:29:46
+ * @LastEditTime: 2020-06-07 16:30:33
  */
 import React, { useState, useEffect } from 'react';
 import { connect, history } from 'umi';
 import { useMount, useUnmount } from '@umijs/hooks';
 import ReactMarkdown from 'react-markdown';
 import 'github-markdown-css';
-import { Drawer, Breadcrumb, Avatar, Button, Tooltip, Comment } from 'antd';
+import {
+  Drawer,
+  Breadcrumb,
+  Avatar,
+  Button,
+  Tooltip,
+  Comment,
+  Skeleton,
+} from 'antd';
 import moment from 'moment';
 import CodeBlock from '../../../components/CodeBlock';
 import hljs from 'highlight.js';
@@ -42,6 +50,7 @@ const ArticleDetail = (props) => {
     match,
     dispatch,
     discuss: { discussDetail },
+    fetchDiscussDetailLoading,
   } = props;
 
   const [commentDetail, setCommentDetail] = useState('');
@@ -115,150 +124,190 @@ const ArticleDetail = (props) => {
   return (
     <div className={styles.main}>
       <div className={styles.main_content}>
-        <div className={styles.article_header}>
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <a
-                href=""
-                onClick={(e) => callChangeCommunicationQuery(e, 'article', '')}
-              >
-                讨论
-              </a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <a
-                href=""
-                onClick={(e) =>
-                  callChangeCommunicationQuery(
-                    e,
-                    'article',
-                    discussDetail.category,
-                  )
-                }
-              >
-                {categoryToCN(discussDetail.category)}
-              </a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>{discussDetail.title}</Breadcrumb.Item>
-          </Breadcrumb>
-        </div>
-        <div className={styles.article_info}></div>
-        <div className={styles.article_content}>
-          <div className="markdown-body">
-            <ReactMarkdown
-              source={discussDetail.detail}
-              escapeHtml={false}
-              renderers={{ code: CodeBlock }}
-            />
+        <Skeleton
+          avatar
+          active
+          paragraph={{ rows: 10 }}
+          loading={fetchDiscussDetailLoading}
+        >
+          <div className={styles.article_header}>
+            <Breadcrumb>
+              <Breadcrumb.Item>
+                <a
+                  href=""
+                  onClick={(e) =>
+                    callChangeCommunicationQuery(e, 'article', '')
+                  }
+                >
+                  讨论
+                </a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <a
+                  href=""
+                  onClick={(e) =>
+                    callChangeCommunicationQuery(
+                      e,
+                      'article',
+                      discussDetail.category,
+                    )
+                  }
+                >
+                  {categoryToCN(discussDetail.category)}
+                </a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>{discussDetail.title}</Breadcrumb.Item>
+            </Breadcrumb>
           </div>
-        </div>
-        <div className={styles.shadow} style={{ height: '20px' }}></div>
-        <div>
-          {discussDetail.comments && discussDetail.comments.length} 条评论
-        </div>
-        <div className={styles.make_comment}>
-          <div className={styles.editor}>
-            <MdEditor
-              className={styles.comment_editor}
-              config={{
-                view: {
-                  menu: false,
-                  md: true,
-                  html: false,
-                },
-              }}
-              style={{ height: '100px', width: '100%' }}
-              value={commentDetail}
-              renderHTML={(text) => mdParser.render(text)}
-              onChange={handleEditorChange}
-            />
-            <div className={styles.comment_button}>
-              <Button className={styles.submit_btn} size="small">
-                评论
+          <div className={styles.article_title}>
+            <Avatar
+              src={
+                (discussDetail.authorInfo && discussDetail.authorInfo.avatar) ||
+                ''
+              }
+              size={32}
+            ></Avatar>
+            <div className={styles.title_info}>{discussDetail.title}</div>
+            <div className={styles.btn_wrapper}>
+              <Button onClick={showMapDrawer}>
+                <ApartmentOutlined /> 查看脑图
               </Button>
             </div>
           </div>
-          <div className={styles.comment_list}>
-            {discussDetail._id &&
-              discussDetail.comments.map((comment) => (
-                <div>
-                  <Comment
-                    actions={[
-                      <span
-                        key="comment-list-reply-to-0"
-                        onClick={() => showcurrentCommentReplyBox(comment)}
-                      >
-                        回复
-                      </span>,
-                    ]}
-                    key={comment._id}
-                    className={[styles.comment].join(' ')}
-                    author={
-                      <span className={styles.name}>
-                        {comment.authorInfo.name}
-                      </span>
-                    }
-                    avatar={
-                      <Avatar
-                        src={comment.authorInfo.avatar}
-                        alt={comment.authorInfo.name}
-                      />
-                    }
-                    content={
-                      <div className="markdown-body">
-                        <ReactMarkdown
-                          source={comment.content}
-                          escapeHtml={false}
-                        />
-                      </div>
-                    }
-                    datetime={
-                      <Tooltip
-                        title={moment(comment.createdAt)
-                          .locale('zh-cn')
-                          .format('YYYY-MM-DD HH:mm:ss')}
-                      >
-                        <span>
-                          {moment(comment.createdAt).locale('zh-cn').fromNow()}
-                        </span>
-                      </Tooltip>
-                    }
-                  />
-                  <div key={comment._id + '123'}>
-                    {comment._id === currentCommentReplyBox && (
-                      <div className={styles.replybox}>
-                        <MdEditor
-                          config={{
-                            view: {
-                              menu: false,
-                              md: true,
-                              html: false,
-                            },
-                          }}
-                          className={styles.comment_editor}
-                          style={{ height: '100px', width: '100%' }}
-                          value={replyDetail}
-                          renderHTML={(text) => mdParser.render(text)}
-                          onChange={handleReplyEditorChange}
-                        />
-                        <div className={styles.comment_button}>
-                          <Button className={styles.submit_btn} size="small">
-                            评论
-                          </Button>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              ))}
+          <div className={styles.info_wrapper}>
+            <div className={styles.article_info}>
+              <span className={styles.author_name}>
+                {(discussDetail.authorInfo && discussDetail.authorInfo.name) ||
+                  ''}
+              </span>
+              <span className={styles.info_text}>
+                {`${discussDetail.access_number} 次阅读`}
+              </span>
+              <span className={styles.dot}></span>
+              <span className={styles.info_text}>
+                {moment(discussDetail.createdAt).lang('zh-cn').fromNow()}
+              </span>
+            </div>
           </div>
-        </div>
+          <div className={styles.article_content}>
+            <div className="markdown-body">
+              <ReactMarkdown
+                source={discussDetail.detail}
+                escapeHtml={false}
+                renderers={{ code: CodeBlock }}
+              />
+            </div>
+          </div>
+          <div className={styles.shadow} style={{ height: '20px' }}></div>
+          <div>
+            {discussDetail.comments && discussDetail.comments.length} 条评论
+          </div>
+          <div className={styles.make_comment}>
+            <div className={styles.editor}>
+              <MdEditor
+                className={styles.comment_editor}
+                config={{
+                  view: {
+                    menu: false,
+                    md: true,
+                    html: false,
+                  },
+                }}
+                style={{ height: '100px', width: '100%' }}
+                value={commentDetail}
+                renderHTML={(text) => mdParser.render(text)}
+                onChange={handleEditorChange}
+              />
+              <div className={styles.comment_button}>
+                <Button className={styles.submit_btn} size="small">
+                  评论
+                </Button>
+              </div>
+            </div>
+            <div className={styles.comment_list}>
+              {discussDetail._id &&
+                discussDetail.comments.map((comment) => (
+                  <div>
+                    <Comment
+                      actions={[
+                        <span
+                          key="comment-list-reply-to-0"
+                          onClick={() => showcurrentCommentReplyBox(comment)}
+                        >
+                          回复
+                        </span>,
+                      ]}
+                      key={comment._id}
+                      className={[styles.comment].join(' ')}
+                      author={
+                        <span className={styles.name}>
+                          {comment.authorInfo.name}
+                        </span>
+                      }
+                      avatar={
+                        <Avatar
+                          src={comment.authorInfo.avatar}
+                          alt={comment.authorInfo.name}
+                        />
+                      }
+                      content={
+                        <div className="markdown-body">
+                          <ReactMarkdown
+                            source={comment.content}
+                            escapeHtml={false}
+                          />
+                        </div>
+                      }
+                      datetime={
+                        <Tooltip
+                          title={moment(comment.createdAt)
+                            .locale('zh-cn')
+                            .format('YYYY-MM-DD HH:mm:ss')}
+                        >
+                          <span>
+                            {moment(comment.createdAt)
+                              .locale('zh-cn')
+                              .fromNow()}
+                          </span>
+                        </Tooltip>
+                      }
+                    />
+                    <div key={comment._id + '123'}>
+                      {comment._id === currentCommentReplyBox && (
+                        <div className={styles.replybox}>
+                          <MdEditor
+                            config={{
+                              view: {
+                                menu: false,
+                                md: true,
+                                html: false,
+                              },
+                            }}
+                            className={styles.comment_editor}
+                            style={{ height: '100px', width: '100%' }}
+                            value={replyDetail}
+                            renderHTML={(text) => mdParser.render(text)}
+                            onChange={handleReplyEditorChange}
+                          />
+                          <div className={styles.comment_button}>
+                            <Button className={styles.submit_btn} size="small">
+                              评论
+                            </Button>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </Skeleton>
       </div>
-      <div className={styles.fixedWeight}>
+      {/* <div className={styles.fixedWeight}>
         <div className={styles.fixedButton} onClick={showMapDrawer}>
           <ApartmentOutlined />
         </div>
-      </div>
+      </div> */}
       <Drawer
         width="1200px"
         title="文章脑图"
